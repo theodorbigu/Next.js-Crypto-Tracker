a7dfde645c086aef811f504687a97d062864e168
Theodor Bigu <t.bigu@yahoo.com>
2021-06-07 15:47:59 +0300

    FInal

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { motion, AnimatePresence } from 'framer-motion';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps, router }) {
+
+  return( 
+    <AnimatePresence exitBeforeEnter>
+    <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+      pageInitial: {
+        opacity: 0
+      },
+      pageAnimate: {
+        opacity: 1
+      },
+      pageExit: {
+        backgroundColor: '#7b8186',
+        opacity: 0
+      }
+    }}>
+    <Component {...pageProps} />
+    </motion.div>
+    </AnimatePresence>
+    )
+
 }
 
 export default MyApp
